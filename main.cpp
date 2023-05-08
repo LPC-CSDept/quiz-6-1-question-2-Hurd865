5b6f8550740c9faef8c10b46ece0bd4547048072
Your Name <cdhurd@zonemail.clpccd.edu>
2023-05-07 20:20:36 -0700

    submit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
+#include <iostream>
 #include "q2.hpp"
-#include <iomanip>
+
 using namespace std;
 
-//********************
-int main()
+void getInput(int &n1, int &n2, int &n3)
 {
-	int n1, n2, n3;
-	int min;
-	getInput(n1, n2, n3);
-	min = findMin(n1, n2, n3);
-	printResult(n1, n2, n3, min);
+    cout << "Enter three numbers: ";
+    cin >> n1 >> n2 >> n3;
+}
+
+int findMin(int n1, int n2, int n3)
+{
+    int min = n1;
+    if (n2 < min)
+    {
+        min = n2;
+    }
+    if (n3 < min)
+    {
+        min = n3;
+    }
+    return min;
+}
+
+void printResult(int n1, int n2, int n3, int min)
+{
+    cout << "The three numbers are: " << n1 << ", " << n2 << ", " << n3 << endl;
+    cout << "The minimum value is: " << min << endl;
 }
